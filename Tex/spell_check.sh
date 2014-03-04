b1d75a8a7ebff6d5992ee649acb5cfed0113b512
kevinkraft <kevin.maguire.2@ucdconnect.ie>
2014-03-04 21:11:45 +0000

    finished notes 6

---
+++ Tex/spell_check.sh
@@ -0,0 +1,8 @@
+aspell -t -c Lorentz_Transform.tex
+aspell -t -c Reparameterise_Schwarzschild_Solution.tex
+aspell -t -c Singular_Lorentz_Transformation.tex
+aspell -t -c Special_Linear_Matrices_of_Lorentz.tex
+aspell -t -c Extended_Complex_Plane.tex
+aspell -t -c Infinitesimal_Lorentz_Transformation.tex
+aspell -t -c main.tex
+
